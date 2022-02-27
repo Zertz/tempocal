4db4c4f94bde2465508c243a84fab26f966609d8
Pier-Luc Gendreau <pierluc@outlook.com>
2022-02-26 20:59:19 -0500

    Update DateTimePicker2.tsx

--- packages/www/examples/DateTimePicker2.tsx
+++ packages/www/examples/DateTimePicker2.tsx
@@ -37,7 +37,7 @@ export function DateTimePicker2({
   return (
     <div className="flex flex-col gap-4">
       <p>{formattedDateTime}</p>
-      <section className="grid grid-rows-[min-content,1fr] grid-cols-[1fr,min-content] gap-2 border border-gray-300 overflow-hidden p-2 rounded w-96 h-[14rem]">
+      <section className="grid grid-rows-[min-content,1fr] grid-cols-[1fr,min-content] gap-2 border border-gray-300 overflow-hidden p-2 rounded w-96 h-[16rem]">
         <header className="flex items-center gap-2 font-bold">
           <button
             className="mr-auto"
