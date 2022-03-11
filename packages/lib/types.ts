92229d4c17de8894f6692010b8e8b000eb04acb3
Pier-Luc Gendreau <pierluc@outlook.com>
2022-03-10 21:44:04 -0500

    Prevent locale from being undefined

--- packages/lib/types.ts
+++ packages/lib/types.ts
@@ -1,5 +1,8 @@
 import { Temporal } from "@js-temporal/polyfill";
 
-export type Locale = Parameters<typeof Intl.DateTimeFormat>[0];
+export type Locale = Exclude<
+  Parameters<typeof Intl.DateTimeFormat>[0],
+  undefined
+>;
 
 export type Value = Temporal.PlainDate | Temporal.PlainDateTime;
