e0e4f16f662d632a97c8cc264f8e3321b6a19809
Pier-Luc Gendreau <pierluc@outlook.com>
2022-03-13 15:03:19 -0400

    Rollover a week if month starts on day 1 or ends on day 7

--- packages/lib/utils.ts
+++ packages/lib/utils.ts
@@ -28,13 +28,13 @@ export function getCalendarMonthDateRange(value: Value, rollover: boolean) {
 
   return {
     start: [0, start.daysInWeek].includes(start.dayOfWeek)
-      ? start
+      ? start.subtract({ days: start.daysInWeek })
       : start.subtract({ days: start.dayOfWeek }),
     end: end.add({
       days:
         end.dayOfWeek === end.daysInWeek
           ? end.daysInWeek - 1
-          : end.daysInWeek - end.dayOfWeek - 1,
+          : end.daysInWeek - end.dayOfWeek - 1 || end.daysInWeek,
     }),
   };
 }
