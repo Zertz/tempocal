5c22b823c1758696cea7da2d88f7789de1165195
Pier-Luc Gendreau <pierluc@outlook.com>
2022-03-28 22:06:25 -0400

    Improve date range example

--- packages/www/components/examples/DateRangePicker.tsx
+++ packages/www/components/examples/DateRangePicker.tsx
@@ -3,7 +3,10 @@ import { Calendar, useTempocal } from "@tempocal/react";
 import classnames from "classnames";
 import * as React from "react";
 
-type DateRange = [Temporal.PlainDate, Temporal.PlainDate];
+type DateRange =
+  | [undefined, undefined]
+  | [Temporal.PlainDate, undefined]
+  | [Temporal.PlainDate, Temporal.PlainDate];
 
 const locale = "en-US";
 
@@ -21,16 +24,17 @@ export function DateRangePicker() {
     }),
   ]);
 
-  const setValue = React.useCallback(
-    (value: typeof values[number]) => {
-      setValues((values) =>
-        Temporal.PlainDate.compare(value, values[0]) < 0
-          ? [value, values[1]]
-          : [values[0], value]
-      );
-    },
-    [setValues]
-  );
+  const [hoverValue, setHoveredValue] = React.useState<Temporal.PlainDate>();
+
+  const setValue = React.useCallback((value: Temporal.PlainDate) => {
+    setValues(([value1, value2]) => {
+      if (value1 && !value2) {
+        return [value1, value].sort(Temporal.PlainDate.compare) as DateRange;
+      }
+
+      return [value, undefined] as DateRange;
+    });
+  }, []);
 
   const { calendarProps, months, onChangeSelectedValue } = useTempocal({
     locale,
@@ -47,12 +51,16 @@ export function DateRangePicker() {
 
   const formattedDates = React.useMemo(() => {
     return [
-      dateFormatter.format(
-        new Date(values[0].year, values[0].month - 1, values[0].day)
-      ),
-      dateFormatter.format(
-        new Date(values[1].year, values[1].month - 1, values[1].day)
-      ),
+      values[0]
+        ? dateFormatter.format(
+            new Date(values[0].year, values[0].month - 1, values[0].day)
+          )
+        : null,
+      values[1]
+        ? dateFormatter.format(
+            new Date(values[1].year, values[1].month - 1, values[1].day)
+          )
+        : null,
     ];
   }, [dateFormatter, values]);
 
@@ -62,41 +70,59 @@ export function DateRangePicker() {
         <Calendar
           {...calendarProps}
           monthsAfter={1}
-          value={values[0]}
           calendarProps={() => ({
             className: "gap-1 text-center w-72",
           })}
           headerProps={() => ({ className: "font-bold" })}
           renderHeader={(date) => months[date.month - 1].longName}
           weekdayProps={() => ({ className: "font-medium" })}
-          renderDay={({ date, disabled, plainDateLike }) => (
-            <button
-              className={classnames(
-                "w-full overflow-hidden rounded border text-gray-700 transition-colors",
-                Temporal.PlainDate.compare(values[0], date) <= 0 &&
-                  Temporal.PlainDate.compare(values[1], date) >= 0
-                  ? "border-blue-600 bg-blue-100"
-                  : "border-gray-300 hover:bg-gray-100"
-              )}
-              disabled={disabled}
-              onClick={() => onChangeSelectedValue(plainDateLike)}
-              type="button"
-            >
-              {date.day}
-            </button>
-          )}
+          renderDay={({ date, disabled, plainDateLike }) => {
+            const isRangeSelected =
+              values[0] &&
+              values[1] &&
+              Temporal.PlainDate.compare(values[0], date) <= 0 &&
+              Temporal.PlainDate.compare(values[1], date) >= 0;
+
+            const isSelected =
+              values[0] && !values[1] && values[0].equals(date);
+
+            const isRangeHovered =
+              values[0] &&
+              !values[1] &&
+              hoverValue &&
+              ((Temporal.PlainDate.compare(values[0], date) <= 0 &&
+                Temporal.PlainDate.compare(hoverValue, date) >= 0) ||
+                (Temporal.PlainDate.compare(hoverValue, date) <= 0 &&
+                  Temporal.PlainDate.compare(values[0], date) >= 0));
+
+            return (
+              <button
+                className={classnames(
+                  "w-full overflow-hidden rounded border text-gray-700 transition-colors",
+                  isRangeSelected || isSelected
+                    ? "border-blue-600 bg-blue-100"
+                    : isRangeHovered
+                    ? "border-blue-300 bg-blue-50"
+                    : "border-gray-300 hover:bg-gray-100"
+                )}
+                disabled={disabled}
+                onClick={() => onChangeSelectedValue(plainDateLike)}
+                onMouseOver={() => setHoveredValue(date)}
+                type="button"
+              >
+                {date.day}
+              </button>
+            );
+          }}
         />
       </div>
       <div className="flex flex-col gap-2 text-sm text-gray-700">
         <p>
-          🚜 🚧{" "}
-          <em>
-            While Tempocal can be used to select a date range, the example needs
-            some love for the user experience to be adequate.
-          </em>
+          🚧 Although this example is still a work in progress, it shows one of
+          the many ways a date range picker can work.
         </p>
         <div>
-          <span className="block font-medium">Selected date</span>
+          <span className="block font-medium">Selected date range</span>
           <span className="mt-1">
             {formattedDates[0]} - {formattedDates[1]}
           </span>
