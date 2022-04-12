7d5f3019673389a1320b145c0c41b31fa8caeee8
Pier-Luc Gendreau <Zertz@users.noreply.github.com>
2022-04-12 08:37:50 -0400

    Add missing exports for dateToTemporal and temporalToDate (#42)
    * Add missing exports for dateToTemporal and temporalToDate
    
    * Create blue-panthers-marry.md

--- packages/core/tempocal-core.ts
+++ packages/core/tempocal-core.ts
@@ -1,3 +1,4 @@
+export * from "./src/dateToTemporal";
 export * from "./src/getCalendarMonthDateRange";
 export * from "./src/getFirstDayOfWeek";
 export * from "./src/getMonthEndDate";
@@ -5,3 +6,4 @@ export * from "./src/getMonths";
 export * from "./src/getMonthStartDate";
 export * from "./src/getWeekdays";
 export * from "./src/getYears";
+export * from "./src/temporalToDate";
