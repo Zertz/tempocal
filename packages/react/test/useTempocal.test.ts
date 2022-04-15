df63b0f94f4b1ab0e6efe243cb083ee623054626
Pier-Luc Gendreau <Zertz@users.noreply.github.com>
2022-04-15 13:04:29 -0400

    Return next value in `onChangeCalendarValue` and `onChangeSelectedValue` (#46)
    * Return next value in onChangeCalendarValue
    
    * Upgrade pnpm
    
    * Return set value in onChangeSelectedValue
    
    * Add useTempocal test
    
    * Update useTempocal.test.ts
    
    * Test values returned by onChange handlers
    
    * Create cyan-months-drop.md

--- packages/react/test/useTempocal.test.ts
+++ packages/react/test/useTempocal.test.ts
@@ -1,5 +1,109 @@
+import { Temporal } from "@tempocal/core/node_modules/@js-temporal/polyfill";
+import { act, renderHook } from "@testing-library/react-hooks";
 import { expect, test } from "vitest";
+import { useTempocal } from "../tempocal-react";
 
-test("nothing", () => {
-  expect(true).toBeTruthy();
+test("useTempocal", () => {
+  let value = Temporal.PlainDate.from({
+    year: 2022,
+    month: 4,
+    day: 15,
+  });
+
+  const { result } = renderHook(() =>
+    useTempocal({
+      locale: "en-US",
+      mode: "date",
+      setValue: (v: Temporal.PlainDate) => (value = v),
+      value,
+    })
+  );
+
+  expect(Object.keys(result.current).length).toBe(6);
+  expect(Object.keys(result.current.calendarProps).length).toBe(4);
+
+  expect(result.current.calendarProps.locale).toBe("en-US");
+  expect(result.current.calendarProps.maxValue).toBeUndefined();
+  expect(result.current.calendarProps.minValue).toBeUndefined();
+
+  expect(
+    result.current.calendarProps.value.equals(
+      Temporal.PlainDate.from({
+        year: 2022,
+        month: 4,
+        day: 15,
+      })
+    )
+  ).toBe(true);
+
+  expect(
+    result.current.calendarValue.equals(
+      Temporal.PlainDate.from({
+        year: 2022,
+        month: 4,
+        day: 15,
+      })
+    )
+  ).toBe(true);
+
+  expect(result.current.months).toHaveLength(12);
+  expect(result.current.years).deep.equal([]);
+
+  // onChangeCalendarValue
+  act(() => {
+    const calendarValue = result.current.onChangeCalendarValue({
+      year: 2023,
+      month: 7,
+    });
+
+    expect(
+      calendarValue.equals(
+        Temporal.PlainDate.from({
+          year: 2023,
+          month: 7,
+          day: 15,
+        })
+      )
+    ).toBe(true);
+  });
+
+  expect(
+    result.current.calendarValue.equals(
+      Temporal.PlainDate.from({
+        year: 2023,
+        month: 7,
+        day: 15,
+      })
+    )
+  ).toBe(true);
+
+  // onChangeSelectedValue
+  act(() => {
+    const selectedValue = result.current.onChangeSelectedValue({
+      year: 2021,
+      month: 1,
+      day: 1,
+    });
+
+    expect(
+      selectedValue instanceof Temporal.PlainDate &&
+        selectedValue.equals(
+          Temporal.PlainDate.from({
+            year: 2021,
+            month: 1,
+            day: 1,
+          })
+        )
+    ).toBe(true);
+  });
+
+  expect(
+    value.equals(
+      Temporal.PlainDate.from({
+        year: 2021,
+        month: 1,
+        day: 1,
+      })
+    )
+  ).toBe(true);
 });
