966029e436ac108651c3c62db204c2928f25e0b4
Pier-Luc Gendreau <pierluc@outlook.com>
2022-04-20 21:30:25 -0400

    Update Sidebar.tsx

--- packages/www/components/Sidebar.tsx
+++ packages/www/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { CalendarIcon, CodeIcon, HomeIcon } from "@heroicons/react/outline";
+import { CalendarIcon, HomeIcon } from "@heroicons/react/outline";
 import classnames from "classnames";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -7,12 +7,6 @@ import { Fragment } from "react";
 const navigation = [
   { name: "Overview", href: "/", icon: HomeIcon, current: true },
   { name: "Examples", href: "/examples", icon: CalendarIcon, current: false },
-  {
-    name: "GitHub",
-    href: "https://github.com/Zertz/tempocal",
-    icon: CodeIcon,
-    current: false,
-  },
 ];
 
 const documentation = [
@@ -109,21 +103,22 @@ export function Sidebar() {
         ))}
       </div>
       <div className="flex-shrink-0 flex border-t border-gray-500 p-4">
-        <a href="#" className="flex-shrink-0 w-full group block">
+        <a
+          href="https://github.com/Zertz/tempocal"
+          className="flex-shrink-0 w-full group block"
+        >
           <div className="flex items-center">
-            <div>
-              <img
-                className="inline-block h-9 w-9 rounded-full"
-                src="https://avatars.githubusercontent.com/u/2636763?v=4"
-                alt=""
-              />
+            <div className="bg-gray-200 group-hover:bg-gray-300 border-2 border-white rounded-full w-10 h-10">
+              <svg viewBox="0 0 16 16" version="1.1">
+                <path
+                  fillRule="evenodd"
+                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+                ></path>
+              </svg>
             </div>
             <div className="ml-3">
               <p className="text-sm font-medium text-gray-200 group-hover:text-gray-300">
-                Pier-Luc Gendreau
-              </p>
-              <p className="text-xs font-medium text-gray-300 group-hover:text-gray-400">
-                View profile
+                Contribute on GitHub
               </p>
             </div>
           </div>
