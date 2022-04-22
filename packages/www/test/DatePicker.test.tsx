a2e5f1d0c994b113059fc2a195cfdd0128da9869
Pier-Luc Gendreau <pierluc@outlook.com>
2022-04-22 19:32:12 -0400

    Update DatePicker.test.tsx

--- packages/www/test/DatePicker.test.tsx
+++ packages/www/test/DatePicker.test.tsx
@@ -3,19 +3,21 @@ import { expect, test } from "vitest";
 import { DatePicker } from "../examples/DatePicker";
 
 test("DatePicker renders without crashing", () => {
-  const { getByText, getByTitle } = render(<DatePicker />);
+  const { getByText, getByTitle } = render(
+    <DatePicker clampCalendarValue locale="en-US" rollover startOfWeek={7} />
+  );
 
-  expect(getByText("November 25, 2021")).toBeTruthy();
+  expect(getByText("Selected date: November 25, 2021")).toBeTruthy();
 
   fireEvent.click(getByText("27"));
-  expect(getByText("November 27, 2021")).toBeTruthy();
+  expect(getByText("Selected date: November 27, 2021")).toBeTruthy();
 
   fireEvent.change(getByTitle("Month"), { target: { value: 2 } });
-  expect(getByText("November 27, 2021")).toBeTruthy();
+  expect(getByText("Selected date: November 27, 2021")).toBeTruthy();
 
   fireEvent.change(getByTitle("Year"), { target: { value: 2022 } });
-  expect(getByText("November 27, 2021")).toBeTruthy();
+  expect(getByText("Selected date: November 27, 2021")).toBeTruthy();
 
   fireEvent.click(getByText("25"));
-  expect(getByText("February 25, 2022")).toBeTruthy();
+  expect(getByText("Selected date: February 25, 2022")).toBeTruthy();
 });
