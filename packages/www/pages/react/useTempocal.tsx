3bc38f671f0c6b275188224969e0f73de70ce909
Pier-Luc Gendreau <pierluc@outlook.com>
2022-04-23 16:04:15 -0400

    Document useTempocal options

--- packages/www/pages/react/useTempocal.tsx
+++ packages/www/pages/react/useTempocal.tsx
@@ -33,6 +33,110 @@ const {
         <div className="mb-4 flex items-end justify-between border-b border-solid border-gray-400 pb-2">
           <h3 className="text-3xl font-bold">Options</h3>
         </div>
+        <div className="space-y-8">
+          <div className="space-y-2">
+            <AnchorHeader id="options-clampCalendarValue">
+              clampCalendarValue
+            </AnchorHeader>
+            <p>
+              When <Code>minValue</Code> and/or <Code>maxValue</Code> are set,
+              automatically keep the calendar value within those values. For
+              example, imagine a date picker with a minimum date in July 2021.
+              If the currently selected date is in March 2022 and you use a year
+              picker to switch to 2021, this should select March 2021. However,
+              since that is earlier than the minimum,{" "}
+              <Code>clampCalendarValue</Code> will automatically select August
+              instead to keep the calendar within the minimum.
+            </p>
+            <CodeBlock>{`clampCalendarValue?: boolean`}</CodeBlock>
+          </div>
+          <div className="space-y-2">
+            <AnchorHeader id="options-clampSelectedValue">
+              clampSelectedValue
+            </AnchorHeader>
+            <p>
+              When <Code>minValue</Code> and/or <Code>maxValue</Code> are set,
+              automatically keep the selected value within those values. For
+              example, imagine a date and time picker with the minimum set to
+              July 15th at 8 AM. If the currently selected date is sometime in
+              the future at 4 AM and you select July 15th, the selected date
+              should become July 15th at 4 AM. However, since that is earlier
+              than the minimum, <Code>clampSelectedValue</Code> will
+              automatically select 8 AM instead to keep the time within the
+              minimum.
+            </p>
+            <CodeBlock>{`clampSelectedValue?: boolean`}</CodeBlock>
+          </div>
+          <div className="space-y-2">
+            <AnchorHeader id="options-locale">locale</AnchorHeader>
+            <p>
+              Any locale supported by the{" "}
+              <ExternalLink href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl#locale_identification_and_negotiation">
+                Intl API
+              </ExternalLink>
+              . Typically a two-letter language code followed by a two-letter
+              country code, separated by a dash. For example,{" "}
+              <Code>"en-US"</Code>, or <Code>"fr-CA"</Code>.
+            </p>
+            <CodeBlock>{`locale: Locale`}</CodeBlock>
+          </div>
+          <div className="space-y-2">
+            <AnchorHeader id="options-maxValue">maxValue</AnchorHeader>
+            <CodeBlock>{`// mode: "date" | "daterange"
+maxValue?: Temporal.PlainDate
+
+// mode: "datetime" | "datetimerange"
+maxValue?: Temporal.PlainDateTime`}</CodeBlock>
+          </div>
+          <div className="space-y-2">
+            <AnchorHeader id="options-minValue">minValue</AnchorHeader>
+            <CodeBlock>{`// mode: "date" | "daterange"
+minValue?: Temporal.PlainDate
+
+// mode: "datetime" | "datetimerange"
+minValue?: Temporal.PlainDateTime`}</CodeBlock>
+          </div>
+          <div className="space-y-2">
+            <AnchorHeader id="options-mode">mode</AnchorHeader>
+            <CodeBlock>{`mode: "date" | "daterange" | "datetime" | "datetimerange"`}</CodeBlock>
+          </div>
+          <div className="space-y-2">
+            <AnchorHeader id="options-setValue">setValue</AnchorHeader>
+            <p>
+              Function to update the selected value, typically the setter
+              returned by <Code>React.useState</Code>.
+            </p>
+            <CodeBlock>{`// mode: "date"
+setValue: (value: Temporal.PlainDate) => void
+
+// mode: "daterange"
+setValue: (value: DateRange) => void
+
+// mode: "datetime"
+setValue: (value: Temporal.PlainDateTime) => void
+
+// mode: "datetimerange"
+setValue: (value: DateTimeRange) => void`}</CodeBlock>
+          </div>
+          <div className="space-y-2">
+            <AnchorHeader id="options-value">value</AnchorHeader>
+            <p>
+              The currently selected value, typically coming from{" "}
+              <Code>React.useState</Code>.
+            </p>
+            <CodeBlock>{`// mode: "date"
+value: Temporal.PlainDate
+
+// mode: "daterange"
+value: DateRange
+
+// mode: "datetime"
+value: Temporal.PlainDateTime
+
+// mode: "datetimerange"
+value: DateTimeRange`}</CodeBlock>
+          </div>
+        </div>
         <div className="mb-4 flex items-end justify-between border-b border-solid border-gray-400 pb-2">
           <h3 className="text-3xl font-bold">Returns</h3>
         </div>
