1f657662ea0a7452f8d6a5449efc4e58a55749cc
Pier-Luc Gendreau <Zertz@users.noreply.github.com>
2026-03-08 11:20:08 -0400

    Delete packages/core/test/getRangeDayState.test.ts

--- packages/core/test/getRangeDayState.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "vitest";
-import { isDateWithinRange } from "../src/isDateWithinRange";
-
-test("getRangeDayState re-exports isDateWithinRange", () => {
-  expect(typeof isDateWithinRange).toBe("function");
-});
